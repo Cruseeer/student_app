22c3c6467268f602452ec4aa24903f2e7aa6d4a1
Cruseeer <cinek005@gmail.com>
2024-01-05 17:37:08 +0100

    zabezpieczenie uzytkownikow haslem

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -65,6 +65,6 @@ class StudentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def student_params
-      params.require(:student).permit(:index, :name)
+      params.require(:student).permit(:index, :name, :password, :password_confirmation)
     end
 end
